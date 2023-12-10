32b1f25bb84ea6579927d85de4362c7a34a44091
mehdier0 <mehdierdem0307@gmail.com>
2023-12-10 14:53:25 +0100

    lecture1 Notizen-Eine Einführung in die Programmiersprache

---
+++ Lectures/lecture1-Introduction.c
@@ -0,0 +1,71 @@
+/*
+    Eigenschaften der Programmiersprache C:
+
+    - imperative            - efficient
+    - procedural            - artificial
+    - non - proprietary     - standard
+    - middle - level        - portable
+    - static typing         - expressive
+
+
+            IMPERATIVE    VS    DECLARATIVE
+      -------------------------------------------
+      -> imperative: In imperative Sprachen beschreiben Sie,
+      wie eine Aufgabe ausgeführt werden soll. Sie geben Schritt
+      für Schritt an, welche Aktionen der Computer ausführen
+      soll. Sie steuern den Programmfluss (flow) mit
+      Schleifen, Bedingungen, ...
+      C ist eine imperative language
+
+      -> declarative: In deklarativen Sprachen beschreiben Sie,
+      was Sie erreichen möchten, ohne detailliert anzugeben,
+      wie es erreicht werden soll.
+         z.B. SQL
+
+
+                PROCEDURAL
+            -----------------
+    In der prozeduralen Technik wird das Programm
+    geschrieben, indem Unterprogramme sich gegenseitig
+    aufrufen.
+
+    In prozeduralen Sprachen werden Programme
+    hauptsächlich als eine Sammlung von Prpzeduren
+    (Funktionen) organisiert.
+
+    In C sind Funktionen ein grundlegender Baustein der
+    Programmstruktur. Sie können Funktionen definieren,
+    die Aufgaben ausführen und diese Funktionen können
+    Parameter akzeptieren und Werte zurückgeben.
+
+
+            MIDDLE - LEVEL
+         ----------------------
+    C ist eine middle - level language, was heißt das?
+        -> "C ist nicht allzu weit weg von der Maschine,
+        aber auch nicht allzuweit entfernt vom Maschinen"
+
+
+
+
+                STATIC   -    DYNAMIC
+                        TYPING
+            -------------------------------
+        static: Eine Sprachen ist statisch typisiert, wenn
+        der Typ einer Variablen zur Kompilierzeit und
+        nicht zur Laufzeit (run time) bekannt ist.
+        Zur Kompilierzeit wird der Quellcode in einer
+        bestimmten Programmiersprache in ein
+        maschinenlesbares Format umgewandelt. Das bedeutet,
+        dass vor der Kompilierung des Quellcodes (source code)
+        der Typ jedes einzelnen Variablen bekannt sein muss.
+
+        dynamic: Eine Sprache ist dynamic typisiert, wenn
+        der Typ einer Variablen während der run time
+        überprüft wird.
+
+
+ */
+
+
+
